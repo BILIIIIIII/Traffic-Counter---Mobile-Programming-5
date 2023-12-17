8d68acdabef3c87eb05159abca04857eeb4d810d
Abil Khairi <113699932+BILIIIIIII@users.noreply.github.com>
2023-12-17 16:25:22 +0700

    Update detail.js - a

--- script/detail.js
+++ script/detail.js
@@ -1,5 +1,3 @@
-let trafficData = [];
-
 const vehicleIcon = {
   motorLeft: "../assets/icon/motor-kiri.png",
   mobilLeft: "../assets/icon/car-kiri.png",
@@ -11,59 +9,69 @@ const vehicleIcon = {
   busRight: "../assets/icon/bus-kanan.png",
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Memeriksa apakah ada data yang disimpan di local storage saat halaman dimuat
-  const storedTrafficData = localStorage.getItem("trafficData");
-  console.log(storedTrafficData);
+const API_ENDPOINT = "https://657df4193e3f5b1894635fae.mockapi.io/trafficData";
+
+document.addEventListener("DOMContentLoaded", async function () {
   const cards = document.getElementById("cards");
+  // const clearDataButton = document.getElementById("clearDataButton");
 
-  if (storedTrafficData) {
-    trafficData = [...JSON.parse(storedTrafficData)]; // Menggabungkan dengan data dari local storage
-    displayTrafficDetails();
+  try {
+    const response = await fetch(API_ENDPOINT);
+    if (!response.ok) {
+      throw new Error("Terjadi kesalahan saat mengambil data.");
+    }
+
+    const trafficData = await response.json();
+
+    if (trafficData.length === 0) {
+      const noDataText = document.createElement("p");
+      noDataText.textContent = "Belum ada data.";
+      noDataText.classList.add("noDataText");
+      cards.appendChild(noDataText);
+
+      const addButton = document.createElement("button");
+      addButton.innerHTML = `
+      <a href="../index.html"><button id="btn-tambah">Tambah Data</button></a>
+      `;
+
+      cards.appendChild(addButton);
+
+      // clearDataButton.style.display = "none";
+    } else {
+      // clearDataButton.style.display = "block";
+      displayTrafficDetails(trafficData);
+    }
+  } catch (error) {
+    showToast(error.message);
   }
 
-  function getTimestamp() {
-    const now = new Date();
+  async function deleteTrafficData(id) {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/${id}`, {
+        method: "DELETE",
+      });
 
-    const jam = `
-      ${now.getHours().toString().padStart(2, "0")}
-        :
-      ${now.getMinutes().toString().padStart(2, "0")}
-        `;
+      if (!response.ok) {
+        throw new Error("Gagal menghapus data dari API.");
+      }
 
-    const hari = getDayName(now.getDay());
-
-    const tanggal = `
-      ${now.getDate().toString().padStart(2, "0")}
-      /
-      ${(now.getMonth() + 1).toString().padStart(2, "0")}
-      /
-      ${now.getFullYear()}
-        `;
-
-    return { jam, hari, tanggal };
+      // Hapus card dari halaman setelah berhasil menghapus dari API
+      const cardToRemove = document.querySelector(`.card[id="${id}"]`);
+      if (cardToRemove) {
+        cardToRemove.parentNode.removeChild(cardToRemove);
+        showToast("Data berhasil dihapus");
+        await displayUpdatedTrafficDetails();
+      }
+    } catch (error) {
+      showToast(error.message);
+    }
   }
 
-  function getDayName(dayIndex) {
-    const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
-    return days[dayIndex];
-  }
-
-  function displayTrafficDetails() {
+  function displayTrafficDetails(trafficData) {
     trafficData.forEach((data) => {
       const card = document.createElement("div");
       card.classList.add("card");
       card.id = `${data.id}`;
-
-      const motorLeft = JSON.stringify(data.vehicle.motorLeft);
-      const mobilLeft = JSON.stringify(data.vehicle.mobilLeft);
-      const trukLeft = JSON.stringify(data.vehicle.trukLeft);
-      const busLeft = JSON.stringify(data.vehicle.busLeft);
-
-      const motorRight = JSON.stringify(data.vehicle.motorRight);
-      const mobilRight = JSON.stringify(data.vehicle.mobilRight);
-      const trukRight = JSON.stringify(data.vehicle.trukRight);
-      const busRight = JSON.stringify(data.vehicle.busRight);
 
       card.innerHTML = ` 
           <div class="time">
@@ -79,74 +87,174 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="lv">
                 <div class="item motor-l">
                     <img src="${vehicleIcon.motorLeft}" alt="Motor Kiri">
-                    <span class="value">${motorLeft}</span>
+                    <span class="value">${data.vehicle.motorLeft}</span>
                 </div>
                 <div class="item mobil-l">
                     <img src="${vehicleIcon.mobilLeft}" alt="Mobil Kiri">
-                    <span class="value">${mobilLeft}</span>
+                    <span class="value">${data.vehicle.mobilLeft}</span>
                 </div>
                 <div class="item truk-l">
                     <img src="${vehicleIcon.trukLeft}" alt="Truk Kiri">
-                    <span class="value">${trukLeft}</span>
+                    <span class="value">${data.vehicle.trukLeft}</span>
                 </div>
                 <div class="item bus-l">
                     <img src="${vehicleIcon.busLeft}" alt="Bus Kiri">
-                    <span class="value">${busLeft}</span>
+                    <span class="value">${data.vehicle.busLeft}</span>
                 </div>
             </div>
             <div class="rv">
                 <div class="item motor-r">
                     <img src="${vehicleIcon.motorRight}" alt="Motor Kanan">
-                    <span class="value">${motorRight}</span> 
+                    <span class="value">${data.vehicle.motorRight}</span> 
                 </div>
                 <div class="item mobil-r">
                     <img src="${vehicleIcon.mobilRight}" alt="Mobil Kanan">
-                    <span class="value">${mobilRight}</span> 
+                    <span class="value">${data.vehicle.mobilRight}</span> 
                 </div>
                 <div class="item truk-r">
                     <img src="${vehicleIcon.trukRight}" alt="Truk Kanan">
-                    <span class="value">${trukRight}</span> 
+                    <span class="value">${data.vehicle.trukRight}</span> 
                 </div>
                 <div class="item bus-r">
                     <img src="${vehicleIcon.busRight}" alt="Bus Kanan">
-                    <span class="value">${busRight}</span> 
+                    <span class="value">${data.vehicle.busRight}</span> 
                 </div>
             </div>
+
+             <button class="deleteButton" >Delete</button>
           </div>
 
       `;
+
+      const deleteButton = card.querySelector(".deleteButton");
+      deleteButton.setAttribute("data-id", data.id);
+
+      deleteButton.addEventListener("click", (event) => {
+        const id = event.target.getAttribute("data-id");
+        deleteTrafficData(id); // Menghapus data berdasarkan ID
+      });
 
       cards.appendChild(card);
     });
   }
 
-  // Menambahkan event listener pada tombol "Clear All"
-  const clearDataButton = document.getElementById("clearDataButton");
-  clearDataButton.addEventListener("click", function () {
-    confirmClearAllTrafficData();
-  });
+  async function displayUpdatedTrafficDetails() {
+    const cards = document.getElementById("cards");
+    cards.innerHTML = ""; // Menghapus semua card yang ada
 
-  function confirmClearAllTrafficData() {
-    // Menampilkan pesan konfirmasi
-    const confirmation = confirm("Apakah Anda yakin ingin menghapus semua data lalu lintas?");
-    if (confirmation) {
-      clearAllTrafficData();
+    try {
+      const response = await fetch(API_ENDPOINT);
+      if (!response.ok) {
+        throw new Error("Terjadi kesalahan saat mengambil data.");
+      }
+
+      const trafficData = await response.json();
+      trafficData.forEach((data, index) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+        <div class="time">
+            <span> 
+              ${data.timeStamp.jam},
+              ${data.timeStamp.hari},
+              ${data.timeStamp.tanggal}
+            </span>
+         
+          </div>
+
+          <div class="v">
+            <div class="lv">
+                <div class="item motor-l">
+                    <img src="${vehicleIcon.motorLeft}" alt="Motor Kiri">
+                    <span class="value">${data.vehicle.motorLeft}</span>
+                </div>
+                <div class="item mobil-l">
+                    <img src="${vehicleIcon.mobilLeft}" alt="Mobil Kiri">
+                    <span class="value">${data.vehicle.mobilLeft}</span>
+                </div>
+                <div class="item truk-l">
+                    <img src="${vehicleIcon.trukLeft}" alt="Truk Kiri">
+                    <span class="value">${data.vehicle.trukLeft}</span>
+                </div>
+                <div class="item bus-l">
+                    <img src="${vehicleIcon.busLeft}" alt="Bus Kiri">
+                    <span class="value">${data.vehicle.busLeft}</span>
+                    </div>
+                    </div>
+                    <div class="rv">
+                <div class="item motor-r">
+                    <img src="${vehicleIcon.motorRight}" alt="Motor Kanan">
+                    <span class="value">${data.vehicle.motorRight}</span> 
+                    </div>
+                <div class="item mobil-r">
+                    <img src="${vehicleIcon.mobilRight}" alt="Mobil Kanan">
+                    <span class="value">${data.vehicle.mobilRight}</span> 
+                    </div>
+                    <div class="item truk-r">
+                    <img src="${vehicleIcon.trukRight}" alt="Truk Kanan">
+                    <span class="value">${data.vehicle.trukRight}</span> 
+                </div>
+                <div class="item bus-r">
+                    <img src="${vehicleIcon.busRight}" alt="Bus Kanan">
+                    <span class="value">${data.vehicle.busRight}</span> 
+                </div>
+            </div>
+
+             <button class="deleteButton" >Delete</button>
+          </div>
+      `;
+
+        const deleteButton = card.querySelector(".deleteButton");
+        deleteButton.setAttribute("data-id", data.id);
+
+        deleteButton.addEventListener("click", (event) => {
+          const id = event.target.getAttribute("data-id");
+          deleteTrafficData(id); // Menghapus data berdasarkan ID
+        });
+
+        cards.appendChild(card);
+      });
+    } catch (error) {
+      showToast(error.message);
     }
   }
 
-  function clearAllTrafficData() {
-    // Menghapus semua data trafficData dari local storage
-    localStorage.removeItem("trafficData");
+  // // Menambahkan event listener pada tombol "Clear All"
+  // clearDataButton.addEventListener("click", function () {
+  //   clearAllTrafficData();
+  // });
 
-    // Mengosongkan array trafficData
-    trafficData = [];
+  // function confirmClearAllTrafficData() {
+  //   // Menampilkan pesan konfirmasi
+  //   const confirmation = confirm("Apakah Anda yakin ingin menghapus semua data lalu lintas?");
+  //   if (confirmation) {
+  //     clearAllTrafficData();
+  //   }
+  // }
 
-    // Menghapus semua elemen kartu pada halaman
-    const cards = document.getElementById("cards");
+  // async function clearAllTrafficData() {
+  //   try {
+  //     const response_DELETE = await fetch("https://657df4193e3f5b1894635fae.mockapi.io/trafficData", {
+  //       method: "DELETE", // Menggunakan metode DELETE untuk menghapus data
+  //     });
 
-    cards.innerHTML = ""; // Mengosongkan elemen cards
-    showToast("Semua data lalu lintas telah dihapus.");
-  }
+  //     if (response_DELETE.ok) {
+  //       // Mengosongkan array trafficData
+  //       trafficData = [];
+
+  //       // Menghapus semua elemen kartu pada halaman
+  //       const cards = document.getElementById("cards");
+  //       cards.innerHTML = ""; // Mengosongkan elemen cards
+
+  //       showToast("Semua data lalu lintas telah dihapus dari API.");
+  //     } else {
+  //       throw new Error("Gagal menghapus data lalu lintas dari API.");
+  //     }
+  //   } catch (error) {
+  //     showToast(error.message);
+  //   }
+  // }
 });
 
 function showToast(message) {
